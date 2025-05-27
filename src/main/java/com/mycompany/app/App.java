13cc205447f4ff0cbc613dedcbdfc039688859b9
prudhvi75 <m.vprudhvi7@gmail.com>
2025-05-27 16:16:20 +0530

    Changed App.Java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World! This is 2025 and i am practicing maven";
 
     public App() {}
 
